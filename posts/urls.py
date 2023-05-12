4877ff2872480416a504dd077ba2da33f6abe47e
eunsu02 <regulus_leo08@naver.com>
2023-05-12 11:28:07 +0900

    domain url 수정

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,7 @@ from posts.views import *
 
 urlpatterns = [
     path('new/', create_post, name = 'create_post'),
-    path('all/', get_all, name = 'get_all'),
+    path('', get_all, name = 'get_all'),
     path('<int:id>/', delete_post, name = 'delete_post'),
     
 ]
